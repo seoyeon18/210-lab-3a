4f78fa14ee8dc7f31b49e04d623b5b0c4bc7c48a
Bright <brightsy04@gmail.com>
2026-02-08 02:16:45 -0800

    Add Restaurant struct

---
+++ restaurant.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Restaurant {
+    string name;
+    string address;
+    string cuisione;
+    int rating;
+    bool reservation;
+};
+
+//function prototypes
+
+int main() {
+    cout << "restaurant struct\n" ;
+    
+
+}
